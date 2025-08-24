23f532de4590171237775cd0f2317069fc429a20
BH267 <hassanbenydir22@gmail.com>
2025-08-24 16:48:17 +0100

    add cpp01/ex05

---
+++ cpp01/ex05/Harl.cpp
@@ -0,0 +1,51 @@
+#include "Harl.hpp"
+#include <iostream>
+#include <ostream>
+
+void	Harl::debug(void)
+{
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!" << std::endl;
+}
+
+void	Harl::info(void)
+{
+	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
+}
+
+void	Harl::warning(void)
+{
+	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month." << std::endl;
+}
+
+void	Harl::error(void)
+{
+	std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
+}
+
+void	Harl::complain(std::string lvl)
+{
+	int	i;
+	std::string lvls[] = {"DEBUG", "INFO", "WARNING", "ERROR"};
+
+	for (i = 0; i < 4 ; i++)
+	{
+		if (lvls[i] == lvl)
+			break;
+	}
+	switch (i) {
+		case 0:
+			debug();
+			break;
+		case 1:
+			info();
+			break;
+		case 2:
+			warning();
+			break;
+		case 3:
+			error();
+			break;
+		default:
+			std::cout << "invalid level \n";
+	}
+}
