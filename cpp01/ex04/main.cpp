e76e1bfdd7ed2f6f6be4a43fbc8e701a91816118
BH267 <hassanbenydir22@gmail.com>
2025-09-22 18:42:40 +0100

    fix some compiling errors in cpp01

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -15,20 +15,18 @@ int	main(int ac, char **av)
 	std::string	s2 = av[3];
 	std::string	line;
 
-	std::ifstream	infile(file);
-	std::ofstream	outfile(file.append(".replace"), std::ios::app);
+	std::ifstream	infile(file.c_str());
+	std::ofstream	outfile(file.append(".replace").c_str(), std::ios::app);
 	while (std::getline(infile, line))
 	{
-		std::cout << line << std::endl;
 		for(int	i = 0; line[i]; i++)
 		{
 			for (int j = 0; line[i + j] && s1[j] && line[i + j] == s1[j]; j++)
 			{
-				if (j + 1 == s1.length())
+				if (j + 1 == (int)s1.length())
 				{
 					line.erase(i, j + 1);
 					line.insert(i, s2);
-					std::cout << "ana hna \n";
 					break;
 				}
 			}
