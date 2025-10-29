6bc49b7a79b6a696591429e280802372c0117450
BH267 <hassanbenydir22@gmail.com>
2025-10-29 20:01:33 +0100

    add cpp05/ex00

--- cpp05/ex00/Bureaucrat.cpp
+++ cpp05/ex00/Bureaucrat.cpp
@@ -2,6 +2,7 @@
 #include <exception>
 #include <iostream>
 #include <ostream>
+#include <string>
 
 Bureaucrat::Bureaucrat() : name("no name"), grade(150)
 {
@@ -10,11 +11,62 @@ Bureaucrat::Bureaucrat() : name("no name"), grade(150)
 
 Bureaucrat::Bureaucrat(std::string n, int g) : name(n)
 {
-	if (g > 150 || 1 > g )
+	if (g < 1)
 		throw GradeTooHighException();
-	std::cout << "default constructor called" << std::endl;
+	else if (g > 150)
+		throw GradeTooLowException();
+	grade = g;
+	std::cout << "a constructor called" << std::endl;
 }
 
-void Bureaucrat::GradeTooHighException() {
-	message("Grade too high");
+Bureaucrat::Bureaucrat(const Bureaucrat& other) : name(other.name), grade(other.grade)
+{
+	std::cout << "copy constructor called" << std::endl;
+}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+	if (this != &other)
+		grade = other.grade;
+	std::cout << "copy assignement called" << std::endl;
+	return *this;
+}
+
+Bureaucrat::~Bureaucrat()
+{
+	std::cout << "destructor called" << std::endl;
+}
+
+
+std::string	Bureaucrat::getName() const {return name;}
+int		Bureaucrat::getGrade() const {return grade;}
+
+void    Bureaucrat::incGrade()
+{
+	if (grade - 1 < 1)
+		throw GradeTooHighException();
+	--grade;
+}
+
+void    Bureaucrat::decGrade()
+{
+	if (grade + 1 > 150)
+		throw GradeTooLowException();
+	++grade;
+}
+
+std::ostream	&operator<<(std::ostream& os, const Bureaucrat& b)
+{
+	os << b.getName() << ", bureaucrat grade " << b.getGrade() << ".";
+	return os;
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw()
+{
+	return "Grade too Low";
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw()
+{
+	return "Grade too High";
 }
--- cpp05/ex00/Bureaucrat.hpp
+++ cpp05/ex00/Bureaucrat.hpp
@@ -4,7 +4,8 @@
 #include <string>
 #include <exception>
 
-class Bureaucrat : public std::exception {
+class Bureaucrat
+{
 private:
 	std::string const	name;
 	int			grade;
@@ -16,16 +17,24 @@ public:
 	Bureaucrat &operator=(const Bureaucrat &);
 	~Bureaucrat();
 
-	void GradeTooHighException();
-	void GradeTooLowException();
-	const char* what() const noexcept;
-
 	std::string	getName() const;
 	int		getGrade() const;
-	void		setName(std::string);
-	void		setGrade(int);
+//	void		setName(std::string);
+//	void		setGrade(int);
 
 	void		incGrade();
 	void		decGrade();
+
+	class GradeTooHighException : public std::exception
+	{
+	public:
+		const char* what() const throw();
+	};
+	class GradeTooLowException : public std::exception
+	{
+	public:
+		const char* what() const  throw();
+	};
+
 };
 std::ostream	&operator<<(std::ostream&, const Bureaucrat&);
